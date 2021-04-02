74b0978a5dd9f72b1dd77c7a8ecd4c6c81784e30
yosoyjacko <jorge_a.correa@hotmail.com>
2021-04-02 14:15:07 -0300

    comentado de api key

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Movie from './component/Movie';
 import styles from './styles/styles.scss'
 
 const FEATURED_API ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
-const IMG_API ="https://image.tmdb.org/t/p/w1280";
+// const IMG_API ="https://image.tmdb.org/t/p/w1280";
 const SEARCH_API ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
 const App = () => {
