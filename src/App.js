50e61a68b03db1f0182183f4a1c61b3c5c5ccb2b
yosoyjacko <jorge_a.correa@hotmail.com>
2021-04-02 03:40:38 -0300

    terminado practica de muestra de peliculas con api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { Fragment } from 'react';
 import { useEffect, useState } from 'react';
 import Movie from './component/Movie';
+import styles from './styles/styles.scss'
 
 const FEATURED_API ="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMG_API ="https://image.tmdb.org/t/p/w1280";
@@ -7,24 +9,52 @@ const SEARCH_API ="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("")
 
   useEffect(()=> {
-   fetch(FEATURED_API)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data)
-      setMovies(data.results)
-    })
-  }, [])
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
+      .then(res => res.json())
+      .then(data => {
+        setMovies(data.results)
+      })  
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm){
+      getMovies(SEARCH_API + searchTerm)
+      setSearchTerm('');
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
 
   return (
-    <div>
-      {movies.length > 0 && movies.map((movie)=> (
-        <Movie key={movie.id} {...movie} />
-      ))
-        
-      }  
-    </div>
+    <Fragment>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input 
+            className="search" 
+            type="search" 
+            placeholder="Search..." 
+            value={searchTerm}
+            onChange={handleOnChange}/>
+        </form>
+      </header>
+      <div className="movie-container">
+        {movies.length > 0 && movies.map((movie)=> (
+          <Movie key={movie.id} {...movie} />
+          ))  
+        }  
+      </div>
+    </Fragment>
   )
 }
 
